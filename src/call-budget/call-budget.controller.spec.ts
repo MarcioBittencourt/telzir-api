b35248abc914ceeac3f134f03b32765e01bb84af
Marcio Bittencourt <marciobitt97@gmail.com>
2022-05-03 17:32:09 -0300

    create test

--- src/call-budget/call-budget.controller.spec.ts
+++ src/call-budget/call-budget.controller.spec.ts
@@ -1,10 +1,14 @@
+import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import * as request from 'supertest';
+
 import { PrismaService } from '../shared/services/prisma.service';
 import { CallBudgetController } from './call-budget.controller';
 import { CallBudgetService } from './call-budget.service';
 
 describe('CallBudgetController', () => {
   let controller: CallBudgetController;
+  let app: INestApplication;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -13,9 +17,36 @@ describe('CallBudgetController', () => {
     }).compile();
 
     controller = module.get<CallBudgetController>(CallBudgetController);
+
+    app = module.createNestApplication();
+    await app.init();
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('/POST franchise', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/franchise/simulate')
+      .send({
+        source: '018',
+        target: '011',
+        duration: 300,
+        amount: 0,
+        amountNoPlan: 0,
+        plan: 'FALEMAIS120',
+      });
+    expect(response.status).toBe(201);
+    expect(response.body.source).toBe('018');
+    expect(response.body.target).toBe('011');
+    expect(response.body.duration).toBe(300);
+    expect(response.body.amount).toBe(376.2);
+    expect(response.body.amountNoPlan).toBe(570);
+    expect(response.body.plan).toBe('FALEMAIS120');
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
